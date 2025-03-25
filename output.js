ce5befe77de3d492f3998d34beedcd315a7649ad
MohammedKShahid <157666536+MohammedKShahid@users.noreply.github.com>
2025-03-24 20:05:09 -0400

    Add files via upload
    week 9 lab

---
+++ output.js
@@ -0,0 +1,15 @@
+function showInnerHTML(){
+	document.getElementById("output1").innerHTML="This message is from innerHTML!";
+}
+
+function useDocumentWrite(){
+	document.write("This changes the content on the page.");
+}
+
+function showAlert(){
+	alert("This is a warning!");
+}
+
+function showConsole(){
+	console.log("The is a message displayed in the console.");
+}
---
+++ variables.js
@@ -0,0 +1,19 @@
+let name = "Mohammed";
+let age = 20;
+let isStudent = true;
+
+document.getElementById("demo1").innerHTML =
+ "Name: "+ name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+var city = "Albany";
+let state = "New York";
+const country = "Canada";
+
+document.getElementById("demo2").innerHTML =
+ "Location: "+ city + ", "+ state + ", " + country; 
+
+let color = "red";
+let color2 = "blue";
+
+document.getElementById("demo3").innerHTML =
+ "My favorite colors are " + color + " and " + color2;
